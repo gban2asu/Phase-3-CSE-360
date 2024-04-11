4fbbb51d62171fc81b729892eef89f7c8901cfc3
Ashur Baroutta <83926352+ashur-gg@users.noreply.github.com>
2024-04-10 22:18:32 -0700

    Update database.java
    removed ID, changed to use name primarily

--- Phase3/src/Phase3/database.java
+++ Phase3/src/Phase3/database.java
@@ -3,17 +3,17 @@ package Phase3;
 import java.io.*;
 import java.util.Scanner;
 
-public class database {
+public class Database {
 
     // Check if patient information file exists
-    public static boolean patientExists(int patientId) {
-        File patientInfoFile = new File(patientId + "_PatientInfo.txt");
+    public static boolean patientExists(String username) {
+        File patientInfoFile = new File(username + "_PatientInfo.txt");
         return patientInfoFile.exists();
     }
 
-    // Retrieve patient's name by ID
-    public static String getPatientName(int patientId) {
-        String patientInfoFileName = patientId + "_PatientInfo.txt";
+    // Retrieve patient's name by username
+    public static String getPatientName(String username) {
+        String patientInfoFileName = username + "_PatientInfo.txt";
         File patientInfoFile = new File(patientInfoFileName);
         if (patientInfoFile.exists()) {
             try (Scanner scanner = new Scanner(patientInfoFile)) {
@@ -35,8 +35,8 @@ public class database {
     }
 
     // Save patient vitals to a file
-    public static void savePatientVitals(int patientId, String... vitals) {
-        String vitalsFileName = patientId + "_Vitals.txt";
+    public static void savePatientVitals(String username, String... vitals) {
+        String vitalsFileName = username + "_Vitals.txt";
         try (PrintWriter out = new PrintWriter(new FileWriter(vitalsFileName, true))) {
             for (String vital : vitals) {
                 out.println(vital);
@@ -47,8 +47,8 @@ public class database {
     }
 
     // Load patient history, contacts, or insurance information
-    public static String loadPatientInfo(int patientId, String infoType) {
-        String fileName = patientId + "_" + infoType + ".txt";
+    public static String loadPatientInfo(String username, String infoType) {
+        String fileName = username + "_" + infoType + ".txt";
         StringBuilder info = new StringBuilder();
         try (Scanner scanner = new Scanner(new File(fileName))) {
             while (scanner.hasNextLine()) {
@@ -62,8 +62,8 @@ public class database {
     }
 
     // Save user credentials and type
-    public static void saveUserCredentials(int userId, String username, String password, String userType) {
-        String credentialsFileName = userId + "_Credentials.txt";
+    public static void saveUserCredentials(String username, String password, String userType) {
+        String credentialsFileName = username + "_Credentials.txt";
         try (PrintWriter out = new PrintWriter(new FileWriter(credentialsFileName))) {
             out.println("Username:" + username);
             out.println("Password:" + password);
@@ -74,8 +74,8 @@ public class database {
     }
 
     // Verify user credentials
-    public static boolean verifyUserCredentials(int userId, String username, String password) {
-        String credentialsFileName = userId + "_Credentials.txt";
+    public static boolean verifyUserCredentials(String username, String password) {
+        String credentialsFileName = username + "_Credentials.txt";
         File credentialsFile = new File(credentialsFileName);
         if (credentialsFile.exists()) {
             try (Scanner scanner = new Scanner(credentialsFile)) {
@@ -95,8 +95,8 @@ public class database {
     }
 
     // Get user type
-    public static String getUserType(int userId) {
-        String credentialsFileName = userId + "_Credentials.txt";
+    public static String getUserType(String username) {
+        String credentialsFileName = username + "_Credentials.txt";
         File credentialsFile = new File(credentialsFileName);
         if (credentialsFile.exists()) {
             try (Scanner scanner = new Scanner(credentialsFile)) {
