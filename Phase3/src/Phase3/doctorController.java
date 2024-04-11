69f9eb1fc6f54d45225343f8378132ef9960961b
Ethan Peterson <epeter34@asu.edu>
2024-04-10 23:07:10 -0700

    Changed buttons to tabs on doctor view

--- Phase3/src/Phase3/doctorController.java
+++ Phase3/src/Phase3/doctorController.java
@@ -1,26 +1,41 @@
 package Phase3;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class doctorController {
 	
+	private Stage primaryStage;
+
 	public void showDoctorPage() {
 		BorderPane root = new BorderPane();
 		HBox topBox = new HBox(20);
 		
 		Label viewLabel = new Label("Doctor Dashboard");
-		Button home = new Button("Home");
-		Button patientHistory = new Button("patientHistory");
-		Button contacts = new Button("Contacts");
-		
+		Tab home = new Tab("Home");
+		Tab patientHistory = new Tab("patientHistory");
+		Tab contacts = new Tab("Contacts");
+		TabPane tabPane = new TabPane();
+
+		tabPane.getTabs().addAll(home, patientHistory, contacts);
 		TextField patientSearch = new TextField();
 		patientSearch.setPromptText("Patient Search");
 		
-		topBox.getChildren().addAll(viewLabel, home, patientHistory, contacts, patientSearch);
-		//TODO implement rest of view
+		topBox.getChildren().addAll(viewLabel, tabPane, patientSearch);
+		
+		primaryStage.setTitle("Doctor Dashboard");
+        Scene scene = new Scene(root, 800, 600);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+		
+		
+
 	}
 }
